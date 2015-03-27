354b0b1c730f3fcae29fd016d82a406b439ff6c7
zwang <zwang@zwang-ubuntu.(none)>
2015-03-27 13:26:40 +0800

    modify for vim.sh

---
+++ conf/vim.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+Common() {
+    echo >> ~/.profile
+    echo "export TERM='xterm-256color'" >> ~/.profile
+    
+    mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
+    
+    sudo pip install jedi
+}
+
+
+Install() {
+    cd ~/.vim/autoload
+    wget https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+    cd ~/.vim/bundle
+    git clone https://github.com/tomtom/tlib_vim.git
+    git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+    git clone https://github.com/garbas/vim-snipmate.git
+    git clone https://github.com/honza/vim-snippets.git
+
+    cd ~/.vim/bundle
+    git clone https://github.com/scrooloose/nerdtree.git
+
+    cd ~/.vim/bundle
+    git clone https://github.com/majutsushi/tagbar.git
+
+    cd ~/.vim/bundle
+    git clone https://github.com/davidhalter/jedi-vim
+    cd jedi-vim
+    git submodule update --init
+
+    cd ~/.vim/colors
+    wget https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow-Night-Bright.vim
+    sed -i 's/^\(let s:comment.*\)/\"\1\nlet s:comment = "cfa8a6"/g' Tomorrow-Night-Bright.vim
+}
+
+Copy() {
+    tar zxvf conf/dotvim.tar.gz -C ~/.vim/
+}
+
+Main() {
+    printf "Do you want to install or copy? (Install/Copy/Skip)? " 
+    read opt
+    case $opt in
+    i|I)
+        Common
+        Install
+    ;;
+    c|C)
+        Common
+        Copy
+    ;;
+    *)
+        echo "Skip ... ..."
+    esac
+    return 0
+}
+
+Main
--- run.sh
+++ run.sh
@@ -4,8 +4,24 @@ AG='sudo apt-get '
 MAX=30
 MASK=50
 
-Apt(){
-    pkg=$1
+os_type='None'
+
+OS() {
+    if [ -e /etc/issue ]; then
+        os_type='ubuntu'
+    elif [ -e /etc/redhat-release ]; then
+         os_type='redhat'
+    elif [ -e /etc/debian_version ]; then
+         os_type='debian'
+    else
+        os_type='unknow'
+    fi
+}
+OS #get operation distribution
+
+Apt() {
+    # install packages for ubuntu
+    local pkg=$1
     printf "Do you want to install %s? (y/No)? " $pkg
     read opt
     case $opt in
@@ -15,6 +31,24 @@ Apt(){
     return 0
 }
 
+Yum() {
+    # install packages for redhat/centos
+    return 0
+}
+
+Install() {
+    local pkg=$1 
+    echo $os_type
+    if [ $os_type == 'ubuntu' ]; then
+        Apt $pkg
+    elif [ $os_type == 'redhat' ]; then
+        Yum $pkg
+    else
+        echo 'do not know dirstribution'
+    fi
+
+}
+
 while [ $MAX -gt 0 ] 
 do
     printf '%*s' $MASK|tr ' ' '*';echo
@@ -22,7 +56,7 @@ do
     echo "* b. Config vim for current user"
     echo "* c. Config ssh for current user"
     echo 
-    echo "* 0. apt-get update for system" 
+    echo "* 0. Upgrade system" 
     echo "* 1. Config sshd for system"
     echo "* 2. Config stunnel for system"
     echo "* 3. Config squid for system"
@@ -39,15 +73,15 @@ do
       $AG update
     ;;
     a)
-      Apt 'screen'
+      Install 'screen'
       cp conf/dot.screenrc ~/.screenrc
     ;;
     b)
-      Apt 'vim'
+      Install 'vim'
       cp conf/dot.vimrc ~/.vimrc
     ;;
     c)
-      Apt 'openssh-client'
+      Install 'openssh-client'
       mkdir -p ~/.ssh
       cp conf/ssh_client.conf ~/.ssh/config
     ;;
