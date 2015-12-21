6b91d9f46efc53e0e72fa27a2df4e90dfa764da7
sastation <sa@station.com>
2015-12-21 08:31:11 +0000

    TERM=linux

--- run.sh
+++ run.sh
@@ -91,7 +91,8 @@ do
     4)
       Install "tmux"
       echo >> ~/.profile
-      echo "export TERM='xterm-256color'" >> ~/.profile
+      #echo "export TERM=xterm-256color" >> ~/.profile
+      echo "export TERM=linux" >> ~/.profile
       echo "alias tmux='tmux -2'" >> ~/.profile
     ;;
     Q)
